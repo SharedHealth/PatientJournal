0b2e8a5ee423df7ff594f7a296509bf7f5fbd58e
rnjn <runjan@gmail.com>
2015-01-22 17:56:46 +0530

    fixing build

--- src/main/java/org/freeshr/journal/launch/ApplicationProperties.java
+++ src/main/java/org/freeshr/journal/launch/ApplicationProperties.java
@@ -1,5 +1,6 @@
 package org.freeshr.journal.launch;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -9,16 +10,11 @@ import static java.lang.System.getenv;
 @Component("patientJournalProperties")
 public class ApplicationProperties {
 
+    @Value("${SHR_SERVER_BASE_URL}")
     private String shrBaseUrl;
 
+    @Value("${IDENTITY_SERVER_URL}")
     private String identityServerUrl;
-
-    public ApplicationProperties() {
-        Map<String, String> env = getenv();
-        shrBaseUrl = env.get("SHR_SERVER_BASE_URL");
-        identityServerUrl = env.get("IDENTITY_SERVER_URL");
-    }
-
 
     public String getIdentityServerUrl(StringBuffer requestURL) {
         return identityServerUrl + "?redirectTo=" + requestURL;
