3bd39382a75c6d1a6ea9442fc3b5e901dcf95196
MD Dubey <mdinfotech007@gmail.com>
2015-07-24 18:39:37 +0530

    Dubey | BDSHR-881 | Fixed issues for family history.

--- src/main/java/org/freeshr/journal/tags/TypeConverter.java
+++ src/main/java/org/freeshr/journal/tags/TypeConverter.java
@@ -1,45 +1,53 @@
 package org.freeshr.journal.tags;
 
+import org.apache.log4j.Logger;
+import org.freeshr.journal.utils.DateUtil;
 import org.hl7.fhir.instance.model.*;
 import org.hl7.fhir.instance.model.Boolean;
 
 public class TypeConverter {
 
-    public static String convertToText(Type typeValue) {
-        if (typeValue == null)
-            return "";
-        if (typeValue.getClass().equals(Quantity.class))
-            return fromQuantity((Quantity) typeValue);
-        if (typeValue.getClass().equals(CodeableConcept.class))
-            return fromCodeableConcept((CodeableConcept) typeValue);
-        if (typeValue.getClass().equals(Ratio.class))
-            return fromRatio((Ratio) typeValue);
-        if (typeValue.getClass().equals(Period.class))
-            return fromPeriod((Period) typeValue);
-        if (typeValue.getClass().equals(SampledData.class))
-            return fromSampleData((SampledData) typeValue);
-        if (typeValue.getClass().equals(String_.class))
-            return fromString_((String_) typeValue);
-        if (typeValue.getClass().equals(Attachment.class))
-            return fromAttachment((Attachment) typeValue);
-        if (typeValue.getClass().equals(Decimal.class))
-            return fromDecimal((Decimal) typeValue);
-        if (typeValue.getClass().equals(Date.class))
-            return fromDate((Date) typeValue);
-        if (typeValue.getClass().equals(Age.class))
-            return fromAge((Age) typeValue);
-        if (typeValue.getClass().equals(DateTime.class))
-            return fromDateTime((DateTime) typeValue);
-        if (typeValue.getClass().equals(Boolean.class))
-            return fromBoolean((Boolean) typeValue);
-        if (typeValue.getClass().equals(Range.class))
-            return fromRange((Range) typeValue);
+    private static Logger logger = Logger.getLogger(TypeConverter.class);
 
+    public static String convertToText(Type typeValue) {
+        try {
+            if (typeValue == null)
+                return "";
+            if (typeValue.getClass().equals(Quantity.class))
+                return fromQuantity((Quantity) typeValue);
+            if (typeValue.getClass().equals(CodeableConcept.class))
+                return fromCodeableConcept((CodeableConcept) typeValue);
+            if (typeValue.getClass().equals(Ratio.class))
+                return fromRatio((Ratio) typeValue);
+            if (typeValue.getClass().equals(Period.class))
+                return fromPeriod((Period) typeValue);
+            if (typeValue.getClass().equals(SampledData.class))
+                return fromSampleData((SampledData) typeValue);
+            if (typeValue.getClass().equals(String_.class))
+                return fromString_((String_) typeValue);
+            if (typeValue.getClass().equals(Attachment.class))
+                return fromAttachment((Attachment) typeValue);
+            if (typeValue.getClass().equals(Decimal.class))
+                return fromDecimal((Decimal) typeValue);
+            if (typeValue.getClass().equals(Date.class))
+                return fromDate((Date) typeValue);
+            if (typeValue.getClass().equals(Age.class))
+                return fromAge((Age) typeValue);
+            if (typeValue.getClass().equals(DateTime.class))
+                return fromDateTime((DateTime) typeValue);
+            if (typeValue.getClass().equals(Boolean.class))
+                return fromBoolean((Boolean) typeValue);
+            if (typeValue.getClass().equals(Range.class))
+                return fromRange((Range) typeValue);
+        } catch (Exception ex) {
+            logger.error(String.format("Unable to parse type-value %s of type %s.", typeValue, typeValue.getClass().getCanonicalName()), ex);
+        }
         return "Unknown";
     }
 
     private static String fromDate(Date typeValue) {
-        return typeValue.getValue().toHumanDisplay();
+        DateAndTime value = typeValue.getValue();
+        return fromDateAndTime(value);
     }
 
     private static String fromDecimal(Decimal typeValue) {
@@ -59,11 +67,17 @@ public class TypeConverter {
     }
 
     private static String fromPeriod(Period typeValue) {
-        return typeValue.getStartSimple().toHumanDisplay() + " to " + typeValue.getEndSimple().toHumanDisplay();
+        String start = (typeValue.getStartSimple() != null) ? fromDateAndTime(typeValue.getStartSimple()) : "Unknown";
+        String end = (typeValue.getEndSimple() != null) ? fromDateAndTime(typeValue.getEndSimple()) : "Ongoing";
+        return start + " to " + end;
     }
 
     private static String fromRatio(Ratio typeValue) {
-        return typeValue.getNumerator().getValueSimple() + "/" + typeValue.getDenominator().getValueSimple();
+        Quantity numerator = typeValue.getNumerator();
+        Quantity denominator = typeValue.getDenominator();
+        if (numerator != null && denominator != null)
+            return numerator.getValueSimple() + "/" + denominator.getValueSimple();
+        return "0";
     }
 
     private static String fromCodeableConcept(CodeableConcept typeValue) {
@@ -76,7 +90,8 @@ public class TypeConverter {
     }
 
     private static String fromDateTime(DateTime typeValue) {
-        return typeValue.getValue().toHumanDisplay();
+        DateAndTime value = typeValue.getValue();
+        return fromDateAndTime(value);
     }
 
     private static String fromAge(Age typeValue) {
@@ -90,6 +105,12 @@ public class TypeConverter {
     private static String fromRange(Range typeValue) {
         Quantity low = typeValue.getLow();
         Quantity high = typeValue.getHigh();
-        return low.getValueSimple() + " " + low.getUnitsSimple() + " - " + high.getValueSimple() + " " + high.getUnitsSimple();
+        String lowerRange = (low != null) ? low.getValueSimple() + " " + low.getUnitsSimple() : "";
+        String higherRange = (high != null) ? high.getValueSimple() + " " + high.getUnitsSimple() : "";
+        return lowerRange + " - " + higherRange;
+    }
+
+    private static String fromDateAndTime(DateAndTime value) {
+        return DateUtil.toDateString(DateUtil.parseDate(value.toString()), "dd MMM yyyy HH:mm");
     }
 }
---
+++ src/test/java/org/freeshr/journal/tags/TypeConverterTest.java
@@ -0,0 +1,66 @@
+package org.freeshr.journal.tags;
+
+import org.hl7.fhir.instance.model.*;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.freeshr.journal.tags.TypeConverter.convertToText;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TypeConverterTest {
+
+    @Test
+    public void shouldConvertANullTypeToEmptyString() throws Exception {
+        assertTrue(convertToText(null).isEmpty());
+    }
+
+    @Test
+    public void shouldConvertAQuantityType() throws Exception {
+        Quantity quantity = new Quantity();
+
+        Decimal value = new Decimal();
+        value.setValue(new BigDecimal(12));
+        String_ units = new String_();
+        units.setValue("mg");
+
+        quantity.setValue(value);
+        quantity.setUnits(units);
+
+        assertEquals("12 mg", convertToText(quantity));
+    }
+
+    @Test
+    public void shouldConvertACodeableConcept() throws Exception {
+        CodeableConcept codeableConcept = new CodeableConcept();
+        assertTrue(convertToText(codeableConcept).isEmpty());
+
+        Coding coding = codeableConcept.addCoding();
+        String fever = "Fever";
+        String_ display = new String_();
+        display.setValue(fever);
+
+        coding.setDisplay(display);
+
+        assertEquals(fever, convertToText(codeableConcept));
+    }
+
+    @Test
+    public void shouldConvertADate() throws Exception {
+        Date date = new Date();
+        date.setValue(new DateAndTime("2015-06-17T00:00:00+05:30"));
+
+        String expectedDate = "17 Jun 2015 00:00";
+        assertEquals(expectedDate, convertToText(date));
+    }
+    
+    @Test
+    public void shouldConvertADateTime() throws Exception {
+        DateTime dateTime = new DateTime();
+        dateTime.setValue(new DateAndTime("2015-06-17T00:00:00+05:30"));
+
+        String expectedDate = "17 Jun 2015 00:00";
+        assertEquals(expectedDate, convertToText(dateTime));
+    }
+}
